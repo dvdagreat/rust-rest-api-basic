b3f10fbc00a81ec2348cfa5862e7f27b151eb1e2
Divyesh Ladva <ladoodivyesh@gmail.com>
2026-01-17 07:53:42 +0000

    refactor(database): pass the reference to repositories instead of ownership

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv()?;
 
     let database_connection = db::connection::get_connection().await?;
-    let users_repository = UsersRepository::new(database_connection);
+    let users_repository = UsersRepository::new(database_connection.clone());
     let users_service = UsersService::new(users_repository);
     let users_controller = UsersController::new(users_service);
 
