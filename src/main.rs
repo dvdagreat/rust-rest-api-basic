79242682343c54cbc89975faf7aa1a37349a5203
Divyesh Ladva <ladoodivyesh@gmail.com>
2026-01-17 08:37:59 +0000

    feat(axum): setup basic router and handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::error::Error;
 
+use axum::{Json, Router, response::IntoResponse, routing::get};
+use serde::Serialize;
+
 use crate::{
     controllers::users::UsersController, repositories::users::UsersRepository,
     services::users::UsersService,
@@ -10,6 +13,23 @@ pub mod db;
 pub mod repositories;
 pub mod services;
 
+#[derive(Serialize)]
+struct EndpointResponse {
+    message: String,
+}
+
+async fn get_user_handler() -> impl IntoResponse {
+    let response = EndpointResponse {
+        message: "hello world".to_string(),
+    };
+
+    Json(response)
+}
+
+fn create_app() -> Router {
+    Router::new().route("/users", get(get_user_handler))
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv()?;
@@ -18,6 +38,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let users_repository = UsersRepository::new(database_connection.clone());
     let users_service = UsersService::new(users_repository);
     let users_controller = UsersController::new(users_service);
+
+    let app = create_app();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:1234")
+        .await
+        .expect("Cannot bind the tcp listener");
+
+    axum::serve(listener, app)
+        .await
+        .expect("Cannot start axum app");
 
     Ok(())
 }
