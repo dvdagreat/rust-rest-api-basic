11d038d7140c8d6d13266b137cc09f6b46580790
Divyesh Ladva <ladoodivyesh@gmail.com>
2026-01-19 11:48:59 +0000

    refactor: move database connection code out of create_app function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::sync::Arc;
 
 use axum::Router;
 
+use sea_orm::DatabaseConnection;
+
 use crate::{
     repositories::users::UsersRepository,
     routers::users::create_user_router,
@@ -15,13 +17,11 @@ pub mod dtos;
 pub mod handlers;
 pub mod repositories;
 pub mod response_handling;
+pub mod routers;
 pub mod services;
 pub mod state;
 
-async fn create_app() -> Router {
-    let database_connection = db::connection::get_connection()
-        .await
-        .expect("Cannot Connect to the database! Sorry comeback later! haha");
+fn create_app(database_connection: DatabaseConnection) -> Router {
     let users_repository = Arc::new(UsersRepository::new(database_connection.clone()));
     let users_service = Arc::new(UsersService::new(users_repository));
 
@@ -37,7 +37,11 @@ async fn create_app() -> Router {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv()?;
-    let app = create_app().await;
+    let database_connection = db::connection::get_connection()
+        .await
+        .expect("Sorry! Cannot Connect to the database! Comeback later!");
+
+    let app = create_app(database_connection.clone());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:1234")
         .await
